ad3c11b63a18ea2845474ce19174a2eeb1b9b1b9
marvi <marvinvalle1404@gmail.com>
2026-06-22 11:52:06 -0600

    chore: proyecto inicial OpenXava

---
+++ src/main/java/ni/edu/uam/bfavocabulario/run/bfavocabulario.java
@@ -0,0 +1,16 @@
+package ni.edu.uam.bfavocabulario.run;
+
+import org.openxava.util.*;
+
+/**
+ * Execute this class to start the application.
+ */
+
+public class bfavocabulario {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("bfavocabulario-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
+		AppServer.run("bfavocabulario"); // Use AppServer.run("") to run in root context
+	}
+
+}
